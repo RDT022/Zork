ac5e84a2d93128a3538039750fe89604d83eecca
RDT022 <adamromanowski12@gmail.com>
2022-08-24 15:11:30 -0400

    Zork 1.1 Submission

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,21 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string InputString = Console.ReadLine().Trim().ToUpper();
+
+            if(InputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing!");
+            }
+            else if(InputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized Command.");
+            }
         }
     }
 }
