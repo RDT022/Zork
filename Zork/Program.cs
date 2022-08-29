0e33a6145503c8b4ddc5ed349600fdd7e4d7c6bc
RDT022 <adamromanowski12@gmail.com>
2022-08-29 14:04:13 -0400

    Zork 1.2 Final Refactoring
    Utilizing the ? operand and TryParse method to condense the code down to a single line within the method.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -15,40 +15,7 @@ namespace Zork
 
         private static Commands ToCommand(string commandString)
         {
-            Commands command;
-
-            switch(commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-                
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            }
-
-            return command;
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
     }
 }
