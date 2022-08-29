89db2d7af4297510d93290a1aa928aff819cd058
RDT022 <adamromanowski12@gmail.com>
2022-08-29 13:49:54 -0400

    Zork 1.2 Update
    Updated the Program.cs file.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,20 +8,45 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string InputString = Console.ReadLine().Trim().ToUpper();
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
 
-            if(InputString == "QUIT")
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+
+            if(commandString == "QUIT")
             {
-                Console.WriteLine("Thank you for playing!");
+                command = Commands.QUIT;
             }
-            else if(InputString == "LOOK")
+            else if(commandString == "LOOK")
             {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                command = Commands.LOOK;
+            }
+            else if(commandString == "NORTH")
+            {
+                command = Commands.NORTH;
+            }
+            else if(commandString == "SOUTH")
+            {
+                command = Commands.SOUTH;
+            }
+            else if(commandString == "EAST")
+            {
+                command = Commands.EAST;
+            }
+            else if(commandString == "WEST")
+            {
+                command = Commands.WEST;
             }
             else
             {
-                Console.WriteLine($"Unrecognized Command: {InputString}");
+                command = Commands.UNKNOWN;
             }
+
+            return command;
         }
     }
 }
