38eb21e90bec23a0c4197de0783832394f4bb535
RDT022 <adamromanowski12@gmail.com>
2022-09-07 14:18:18 -0400

    Zork 2.1 Initial Commit
    Adding the array of room names, an index to keep track of the player's position, and the move function.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Zork
 {
     class Program
     {
+        private static string[] Rooms = new string[] { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static byte RoomIndex = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[RoomIndex]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
@@ -18,16 +21,25 @@ namespace Zork
                 switch (command)
                 {
                     case Commands.LOOK:
-                        outputString = "There is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
                     case Commands.QUIT:
                         outputString = "Thank you for playing!";
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
+                        outputString = "The way is shut!";
+                        break;
                     case Commands.WEST:
                     case Commands.EAST:
-                        outputString = $"You moved {command}.";
+                        if(Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
                         break;
                     default:
                         outputString = "Unknown command.";
@@ -41,6 +53,24 @@ namespace Zork
         private static Commands ToCommand(string commandString)
         {
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        }
+
+        private static bool Move(Commands command)
+        {
+            if(command == Commands.WEST && RoomIndex - 1 >= 0)
+            {
+                RoomIndex--;
+                return true;
+            }
+            else if(command == Commands.EAST && RoomIndex + 1 < Rooms.Length)
+            {
+                RoomIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
