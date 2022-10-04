c0086d724cca000800d597a5c1933ee1616d2d86
RDT022 <adamromanowski12@gmail.com>
2022-10-03 21:24:14 -0400

    Zork 4.3 Refactoring

---
+++ Zork/Game.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System;
+
+namespace Zork
+{
+    public class Game
+    {
+        public World World { get; private set; }
+
+        [JsonIgnore]
+        public Player Player { get; private set; }
+
+        [JsonIgnore]
+        private bool IsRunning { get; set; }
+
+        public Game(World world, Player player)
+        {
+           World = world;
+           Player = player;
+        }
+
+        public void Run()
+        {
+            Room previousRoom = null;
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.WriteLine(Player.Location);
+                if (previousRoom != Player.Location)
+                {
+                    Console.WriteLine(Player.Location.Description);
+                    previousRoom = Player.Location;
+                }
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        outputString = Player.Location.Description;
+                        break;
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.WEST:
+                    case Commands.EAST:
+                        Directions direction = Enum.Parse<Directions>(command.ToString(), true);
+                        if (Player.Move(direction))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
+        }
+
+        public static Game Load(string filename)
+        {
+            Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+            game.Player = game.World.SpawnPlayer();
+            return game;
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        }
+    }  
+}
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,109 +7,20 @@ namespace Zork
 {
     class Program
     {
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return _rooms[_location.Row, _location.Column];
-            }
-        }
-
         static void Main(string[] args)
         {
-            const string defaultRoomsFilename = @"Content\Rooms.json";
-            string roomsFileName = (args.Length > 0 ? args[0] : defaultRoomsFilename);
+            const string defaultGameFilename = @"Content\Rooms.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
-            InitializeRoomDescriptions(roomsFileName);
+            Game game = Game.Load(gameFilename);
             Console.WriteLine("Welcome to Zork!");
-
-            Room previousRoom = null;
-            Commands command = Commands.UNKNOWN;
-            while (command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-                if(previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString;
-                switch (command)
-                {
-                    case Commands.LOOK:
-                        outputString = CurrentRoom.Description;
-                        break;
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.WEST:
-                    case Commands.EAST:
-                        if (Move(command))
-                        {
-                            outputString = $"You moved {command}.";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
-                    default:
-                        outputString = "Unknown command.";
-                        break;
-                }
-
-                Console.WriteLine(outputString);
-            }
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
 
-        private static Commands ToCommand(string commandString)
+        private enum CommandLineArguments
         {
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-        }
-
-        private static bool Move(Commands command)
-        {
-            bool didMove;
-            switch (command)
-            {
-                case Commands.NORTH when _location.Row < _rooms.GetLength(0) - 1:
-                    _location.Row++;
-                    didMove = true;
-                    break;
-                case Commands.SOUTH when _location.Row > 0:
-                    _location.Row--;
-                    didMove = true;
-                    break;
-                case Commands.EAST when _location.Column < _rooms.GetLength(1) - 1:
-                    _location.Column++;
-                    didMove = true;
-                    break;
-                case Commands.WEST when _location.Column > 0:
-                    _location.Column--;
-                    didMove = true;
-                    break;
-                default:
-                    didMove = false;
-                    break;
-            }
-            return didMove;
-        }
-
-        private static void InitializeRoomDescriptions(string roomsFilename) =>
-            _rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-
-        private static Room[,] _rooms;
-        private static (int Row, int Column) _location = (1, 1);
-
-        private enum Fields
-        {
-            Name = 0,
-            Description
+            GameFilename = 0
         }
     }
 }
